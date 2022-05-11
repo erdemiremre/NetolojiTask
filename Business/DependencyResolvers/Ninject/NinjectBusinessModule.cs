9d008feaf6f91653fc90de4a4da558f2d63cddb4
erdemiremre <73900322+erdemiremre@users.noreply.github.com>
2022-05-11 13:49:01 +0300

    Ninject kutuphanesi eklendi.

---
+++ Business/DependencyResolvers/Ninject/NinjectBusinessModule.cs
@@ -0,0 +1,25 @@
+﻿using Ninject.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.DependencyResolvers.Ninject
+{
+    public class NinjectBusinessModule : NinjectModule
+    {
+        public override void Load()
+        {
+            //Product for
+
+
+            //User for
+
+
+            //Category for
+
+
+        }
+    }
+}
