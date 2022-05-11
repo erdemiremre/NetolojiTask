b67c1f9c6ae1811709203bc6617369bc20831d54
erdemiremre <73900322+erdemiremre@users.noreply.github.com>
2022-05-11 14:16:34 +0300

    Veritabani baglanti islemleri eklendi.

---
+++ DataAccess/Concrete/AdoNet/Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.AdoNet
+{
+    public class Context
+    {
+        public string Conn { get; set; }
+        public Context()
+        {
+            Conn = ConfigurationManager.ConnectionStrings["DbConnectionString"].ConnectionString;
+        }
+    }
+}
--- NetolojiMvcUI/Global.asax.cs
+++ NetolojiMvcUI/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NetolojiMvcUI.App_Start;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -16,6 +17,7 @@ namespace NetolojiMvcUI
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            ControllerBuilder.Current.SetControllerFactory(new NinjectControllerFactory());
         }
     }
 }
