952821aa813ce9e6ce4fc7dc32b876335a6c4f5d
erdemiremre <73900322+erdemiremre@users.noreply.github.com>
2022-05-11 23:15:32 +0300

    Updated..11.05.2022-23.15

---
+++ Entities/Concrete/Employee.cs
@@ -0,0 +1,17 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class Employee : IEntity
+    {
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Title { get; set; }  //Calisma alanindaki görevi
+    }
+}
