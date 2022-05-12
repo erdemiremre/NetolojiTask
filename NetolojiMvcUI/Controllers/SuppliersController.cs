7e03f8a10a38c54295786c886b8b2c006fbd72a3
erdemiremre <73900322+erdemiremre@users.noreply.github.com>
2022-05-12 17:59:57 +0300

    updated 12.05.2022/18.00

--- NetolojiMvcUI/Controllers/SuppliersController.cs
+++ NetolojiMvcUI/Controllers/SuppliersController.cs
@@ -1,4 +1,5 @@
 ﻿using Business.Abstract;
+using Entities.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,11 +27,38 @@ namespace NetolojiMvcUI.Controllers
             return PartialView("PartialAddSupplier");
         }
 
+        [HttpPost]
+        public JsonResult Add(Supplier supplier)
+        {
+            var result = _supplierService.Add(supplier);
+            return Json(result.Success);
+        }
         public ActionResult GetAll()
         {
             var result = _supplierService.GetAll();
             return PartialView("PartialListSupplier", result.Data);
         }
 
+        [HttpPost]
+        public JsonResult Delete(int id)
+        {
+            var find = _supplierService.Get(id);
+            var result = _supplierService.Delete(find.Data);
+            return Json(result.Success);
+        }
+
+
+        public ActionResult Update(int id)
+        {
+            var result = _supplierService.Get(id);
+            return View(result.Data);
+        }
+
+        [HttpPost]
+        public ActionResult Update(Supplier supplier)
+        {
+            _supplierService.Update(supplier);
+            return RedirectToAction("Index", "Suppliers");
+        }
     }
 }
