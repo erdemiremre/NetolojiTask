5665f03f1cd8844583646180dfe7fb4f82af7bd9
erdemiremre <73900322+erdemiremre@users.noreply.github.com>
2022-05-12 15:25:08 +0300

    added for supplier UI

---
+++ NetolojiMvcUI/Controllers/SuppliersController.cs
@@ -0,0 +1,24 @@
+﻿using Business.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NetolojiMvcUI.Controllers
+{
+    public class SuppliersController : Controller
+    {
+        // GET: Suppliers
+        ISupplierService _supplierService;
+        public SuppliersController(ISupplierService supplierService)
+        {
+            _supplierService = supplierService;
+        }
+        public ActionResult Index()
+        {
+            var result = _supplierService.GetAll();
+            return View(result.Data);
+        }
+    }
+}
